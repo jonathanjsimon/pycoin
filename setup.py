05bc67b40752a0ca40bbc7b32eb743b75ac09c26
Jonathan Simon <simon.jonathan@gmail.com>
2017-08-21 22:25:19 -0400

    Add some .app details.

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup
 
 APP = ['pycoin.py']
+APP_NAME = "PyCoin"
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
         'LSUIElement': True,
+        'CFBundleName': APP_NAME,
+        'CFBundleDisplayName': APP_NAME,
+        'CFBundleGetInfoString': "Monitor cryptos",
+        'CFBundleIdentifier': "com.simon.osx.pycoin",
+        'CFBundleVersion': "0.1.0",
+        'CFBundleShortVersionString': "0.1.0",
+        'NSHumanReadableCopyright': u"Copyright \u00A9 2017, Jonathan Simon, All Rights Reserved"
     },
     'packages': ['rumps', 'requests', 'certifi'],
 }
 
 setup(
     app=APP,
-    name="PyCoin",
+    name=APP_NAME,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app']
